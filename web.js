295e01baa7aca7a151e603ac50a4647b7989a89a
Jeff Steward <steward.jeff@gmail.com>
2013-12-13 14:50:47 -0500

    still working out the responses

--- web.js
+++ web.js
@@ -10,16 +10,17 @@ app.get('/', function(request, response) {
 	twilioResponse.say("Welcome to HAM phone.");
 
 	rest.get("http://api.harvardartmuseums.org/collection/object?s=random&size=1", {timeout:10000})
+		.on("timeout", function(ms) {
+			response.setHeader("Content-Type", "text/xml");
+			response.end(twilioResponse.toString());
+
+		})		
 		.on("complete", function(result) {
 			twilioResponse.say(result.records[0].title);
 
-			response.setHeader("Content-Type", "text/xml")
+			response.setHeader("Content-Type", "text/xml");
 			response.end(twilioResponse.toString());
 		
-		}).on("timeout", function(ms) {
-			response.setHeader("Content-Type", "text/xml")
-			response.end(twilioResponse.toString());
-
 		});
 
 });
