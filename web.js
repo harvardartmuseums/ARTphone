6b8577c9252cb62dfeaaa7b15bf89aede9931772
Jeff Steward <steward.jeff@gmail.com>
2013-12-13 14:19:30 -0500

    initial version including restler for calls to the HAM API

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require("express"),
-	twilio = require("twilio");
+	twilio = require("twilio"),
+	rest = require("restler");
 
 var app = express();
 
@@ -8,8 +9,16 @@ var twilioResponse = new twilio.TwimlResponse();
 app.get('/', function(request, response) {
 	twilioResponse.say("Welcome to HAM phone.");
 
+	rest.get("http://api.harvardartmuseums.org/collection/object?s=random&size=1")
+		.on("complete", function(result) {
+			twilioResponse.say(result.records[0].title);
+
+			response.setHeader("Content-Type", "text/xml")
+			response.end(twilioResponse.toString());
+		});
+
 	response.setHeader("Content-Type", "text/xml")
-	response.send();
+	response.end(twilioResponse.toString());
 });
 
 var port = process.env.PORT || 5000;
