fcbecbcf45d174ebf1fb3c0ca85a9a94a439c778
Jeff Steward <steward.jeff@gmail.com>
2013-12-13 14:24:59 -0500

    testing the response code with a rest call

--- web.js
+++ web.js
@@ -9,16 +9,19 @@ var twilioResponse = new twilio.TwimlResponse();
 app.get('/', function(request, response) {
 	twilioResponse.say("Welcome to HAM phone.");
 
-	rest.get("http://api.harvardartmuseums.org/collection/object?s=random&size=1")
+	rest.get("http://api.harvardartmuseums.org/collection/object?s=random&size=1", {timeout:10000})
 		.on("complete", function(result) {
 			twilioResponse.say(result.records[0].title);
 
 			response.setHeader("Content-Type", "text/xml")
 			response.end(twilioResponse.toString());
+		
+		}).on("timeout", function(ms) {
+			response.setHeader("Content-Type", "text/xml")
+			response.end(twilioResponse.toString());
+
 		});
 
-	response.setHeader("Content-Type", "text/xml")
-	response.end(twilioResponse.toString());
 });
 
 var port = process.env.PORT || 5000;
