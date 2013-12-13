c6465d171d96da194cba2d89aeb3ccc791c744c9
Jeff Steward <steward.jeff@gmail.com>
2013-12-13 14:09:43 -0500

    testing the response code

--- web.js
+++ web.js
@@ -7,10 +7,9 @@ var twilioResponse = new twilio.TwimlResponse();
 
 app.get('/', function(request, response) {
 	twilioResponse.say("Welcome to HAM phone.");
-	response.end();
 
-	//response.setHeader("Content-Type", "text/xml")
-	//response.send(twilioResponse.toString());
+	response.setHeader("Content-Type", "text/xml")
+	response.send();
 });
 
 var port = process.env.PORT || 5000;
